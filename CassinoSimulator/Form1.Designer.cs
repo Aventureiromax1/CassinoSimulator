ffc9a0508f50335620071c834554dec7dca7fd90
Leonardo Godoy dos Anjos <leonardo.g.anjos@unesp.br>
2025-03-27 23:55:17 -0200

    Adicionar arquivos de projeto.

---
+++ CassinoSimulator/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace CassinoSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrSpin = new System.Windows.Forms.Timer(this.components);
+            this.BtnSpin = new System.Windows.Forms.Button();
+            this.lblN1 = new System.Windows.Forms.Label();
+            this.lblN2 = new System.Windows.Forms.Label();
+            this.lblN3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tmrSpin
+            // 
+            this.tmrSpin.Tick += new System.EventHandler(this.tmrSpin_Tick);
+            // 
+            // BtnSpin
+            // 
+            this.BtnSpin.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BtnSpin.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSpin.Location = new System.Drawing.Point(12, 259);
+            this.BtnSpin.Name = "BtnSpin";
+            this.BtnSpin.Size = new System.Drawing.Size(312, 87);
+            this.BtnSpin.TabIndex = 0;
+            this.BtnSpin.Text = "Girar";
+            this.BtnSpin.UseVisualStyleBackColor = true;
+            this.BtnSpin.Click += new System.EventHandler(this.BtnSpin_Click);
+            // 
+            // lblN1
+            // 
+            this.lblN1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblN1.Font = new System.Drawing.Font("Microsoft Sans Serif", 70F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblN1.ForeColor = System.Drawing.Color.Black;
+            this.lblN1.Location = new System.Drawing.Point(12, 84);
+            this.lblN1.Name = "lblN1";
+            this.lblN1.Size = new System.Drawing.Size(100, 100);
+            this.lblN1.TabIndex = 3;
+            this.lblN1.Text = "0";
+            this.lblN1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblN2
+            // 
+            this.lblN2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblN2.Font = new System.Drawing.Font("Microsoft Sans Serif", 70F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblN2.ForeColor = System.Drawing.Color.Black;
+            this.lblN2.Location = new System.Drawing.Point(118, 84);
+            this.lblN2.Name = "lblN2";
+            this.lblN2.Size = new System.Drawing.Size(100, 100);
+            this.lblN2.TabIndex = 4;
+            this.lblN2.Text = "0";
+            this.lblN2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblN3
+            // 
+            this.lblN3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblN3.Font = new System.Drawing.Font("Microsoft Sans Serif", 70F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblN3.ForeColor = System.Drawing.Color.Black;
+            this.lblN3.Location = new System.Drawing.Point(224, 84);
+            this.lblN3.Name = "lblN3";
+            this.lblN3.Size = new System.Drawing.Size(100, 100);
+            this.lblN3.TabIndex = 5;
+            this.lblN3.Text = "0";
+            this.lblN3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(336, 359);
+            this.Controls.Add(this.lblN3);
+            this.Controls.Add(this.lblN2);
+            this.Controls.Add(this.lblN1);
+            this.Controls.Add(this.BtnSpin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrSpin;
+        private System.Windows.Forms.Button BtnSpin;
+        private System.Windows.Forms.Label lblN1;
+        private System.Windows.Forms.Label lblN2;
+        private System.Windows.Forms.Label lblN3;
+    }
+}
+
---
+++ CassinoSimulator/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CassinoSimulator
+{
+    public partial class Form1: Form
+    {
+        int[] roulette;
+        int[] cycles;
+        Label[] screen;
+        Random r;
+        public Form1()
+        {
+            InitializeComponent();
+            roulette = new int[3];
+            cycles = new int[3];
+            screen = new Label[] { lblN1, lblN2, lblN3 };
+            r = new Random();
+            for (int i = 0; i < roulette.Length; i++)
+            {
+                roulette[i] = r.Next(0, 10);
+                Atualizar(i);
+            }
+        }
+        void Atualizar(int i)
+        {
+            screen[i].Text = roulette[i].ToString();
+        }
+
+        private void BtnSpin_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < cycles.Length; i++)
+            {
+                cycles[i] = r.Next(1, 21);
+                screen[i].ForeColor = Color.Black;
+            }
+            Array.Sort(cycles);
+            BtnSpin.Enabled = false;
+            tmrSpin.Enabled = true;
+        }
+        void verifyWin()
+        {
+            if (lblN1.Text == lblN2.Text && lblN2.Text == lblN3.Text)
+            {
+                lblN1.ForeColor = Color.Green;
+                lblN2.ForeColor = Color.Green;
+                lblN3.ForeColor = Color.Green;
+                MessageBox.Show("VOCE GANHOU!!!!");
+            }
+        }
+
+        private void tmrSpin_Tick(object sender, EventArgs e)
+        {
+            bool stopped = true;
+            for (int i = 0; i < cycles.Length; i++)
+            {
+                if (cycles[i] > 0)
+                {
+                    cycles[i]--;
+                    if (cycles[i] == 0)
+                        screen[i].ForeColor = Color.Red;
+                    roulette[i]++;
+                    if (roulette[i] == 10)
+                        roulette[i] = 0;
+                    Atualizar(i);
+                    stopped &= false;
+                }
+                else
+                {
+                    stopped &= true;
+                }
+            }
+            if (stopped)
+            {
+                tmrSpin.Enabled = false;
+                BtnSpin.Enabled = true;
+                verifyWin();
+            }
+        }
+    }
+}
